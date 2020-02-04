c6ae288c086a867d0f39ae31659719d892625418
unknown <joakim.grohn@gmail.com>
2020-02-04 18:44:40 +0100

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBBucvi0yOY2f2LNlsAjFmN_Z81u_L1TDE",
+    authDomain: "fir-test-41ee7.firebaseapp.com",
+    databaseURL: "https://fir-test-41ee7.firebaseio.com",
+    projectId: "fir-test-41ee7",
+    storageBucket: "fir-test-41ee7.appspot.com",
+    messagingSenderId: "197163023154",
+    appId: "1:197163023154:web:1d4c7ea36c23d1c66f752b"
+}
